d54b2eb70d2413e30a2f8ff03b30f3008f912b21
Aditya Singh <mail.adityasingh01@gmail.com>
2024-01-17 03:24:08 +0530

    compressed the javaScript file

--- main.js
+++ main.js
@@ -40,12 +40,9 @@ let bg = document.getElementById("main")
 let calcFrame =document.querySelector(".calculatorFrame")
 let themeBallContainer = document.querySelector(".theme-ball-container");
 
-
-
-themeChangeBtn.forEach(function(elem){
-    elem.addEventListener("click", function(){
-        if(elem.innerHTML == "1"){
-            bg.style.background = `url("img/background elem1.png"), linear-gradient(#FFE77A, #2C5F2D)`;
+// themes
+function theme1(){
+    bg.style.background = `url("img/background elem1.png"), linear-gradient(#FFE77A, #2C5F2D)`;
             bg.style.backgroundPosition = "center";
             bg.style.backgroundRepeat = "no-repeat";
             bg.style.backgroundSize = "contain";
@@ -54,9 +51,9 @@ themeChangeBtn.forEach(function(elem){
         
         
             themeBallContainer.style.justifyContent = "start"
-        }
-        else if(elem.innerHTML == "2"){
-            bg.style.background = `url("img/background elem1.png"), linear-gradient(rgb(0, 123, 148), rgb(8, 0, 63))`;
+};
+function theme2(){
+    bg.style.background = `url("img/background elem1.png"), linear-gradient(rgb(0, 123, 148), rgb(8, 0, 63))`;
             bg.style.backgroundPosition = "center";
             bg.style.backgroundRepeat = "no-repeat";
             bg.style.backgroundSize = "contain";
@@ -65,17 +62,27 @@ themeChangeBtn.forEach(function(elem){
             
             
             themeBallContainer.style.justifyContent = "center"
+};
+function theme3(){bg.style.background = `url("img/background elem1.png"), linear-gradient(#FF69B4, #00FFFF)`;
+bg.style.backgroundPosition = "center";
+bg.style.backgroundRepeat = "no-repeat";
+bg.style.backgroundSize = "contain";
+calcFrame.style.backgroundColor = "rgba(150, 150, 150, 0.314)"
+
+
+
+themeBallContainer.style.justifyContent = "end"}
+
+themeChangeBtn.forEach(function(elem){
+    elem.addEventListener("click", function(){
+        if(elem.innerHTML == "1"){
+            theme1();
+        }
+        else if(elem.innerHTML == "2"){
+            theme2();
         }
         else{
-            bg.style.background = `url("img/background elem1.png"), linear-gradient(#FF69B4, #00FFFF)`;
-            bg.style.backgroundPosition = "center";
-            bg.style.backgroundRepeat = "no-repeat";
-            bg.style.backgroundSize = "contain";
-            calcFrame.style.backgroundColor = "rgba(150, 150, 150, 0.314)"
-        
-        
-            
-            themeBallContainer.style.justifyContent = "end"
+            theme3();
         }
     })
 })
@@ -90,37 +97,13 @@ themeBallContainer.addEventListener("click", function(dets){
     dets.x);
 
     if(positionVal <= 33){
-        bg.style.background = `url("img/background elem1.png"), linear-gradient(#FFE77A, #2C5F2D)`;
-            bg.style.backgroundPosition = "center";
-            bg.style.backgroundRepeat = "no-repeat";
-            bg.style.backgroundSize = "contain";
-            calcFrame.style.backgroundColor = "rgba(200, 200, 200, 0.314)"
-        
-        
-        
-            themeBallContainer.style.justifyContent = "start"
+        theme1();
     }
     else if( positionVal > 33 & positionVal < 66){
-        bg.style.background = `url("img/background elem1.png"), linear-gradient(rgb(0, 123, 148), rgb(8, 0, 63))`;
-        bg.style.backgroundPosition = "center";
-        bg.style.backgroundRepeat = "no-repeat";
-        bg.style.backgroundSize = "contain";
-        calcFrame.style.backgroundColor = "rgba(0, 0, 0, 0.314)"
-        
-        
-        
-        themeBallContainer.style.justifyContent = "center"
+        theme2();
     }
     else{
-        bg.style.background = `url("img/background elem1.png"), linear-gradient(#FF69B4, #00FFFF)`;
-            bg.style.backgroundPosition = "center";
-            bg.style.backgroundRepeat = "no-repeat";
-            bg.style.backgroundSize = "contain";
-            calcFrame.style.backgroundColor = "rgba(150, 150, 150, 0.314)"
-        
-        
-            
-            themeBallContainer.style.justifyContent = "end"
+        theme3();
     }
 
 
